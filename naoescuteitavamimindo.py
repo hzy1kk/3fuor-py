17c7c07fbe403439d1af2e8b9388a692106ecb26
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2025-10-08 16:07:44 +0000

    save

---
+++ naoescuteitavamimindo.py
@@ -0,0 +1,13 @@
+numeros = [10, 5, 23, 7, 15]
+
+print("Maior valor:", max(numeros))
+print("Menor valor:", min(numeros))
+
+
+numeros_ordenados = sorted(numeros)
+print("Lista ordenada:", numeros_ordenados)
+
+print("Quantidade de elementos:", len(numeros))
+ 
+media = sum(numeros) / len(numeros)
+print("Média dos valores:", media)
